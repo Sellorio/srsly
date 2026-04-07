0fdefa4329b407e3f3b77d7ffe385757a2da1664
Sea Millo <sea.millo@agilent.com>
2026-04-07 11:54:17 +1000

    Blazor template

---
+++ Sellorio.Srsly.Web/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,7 @@
+namespace Sellorio.Srsly.Web.Components.Account;
+
+public enum PasskeyOperation
+{
+    Create = 0,
+    Request = 1,
+}
---
+++ Sellorio.Srsly.Web/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Sellorio.Srsly.Web.Data;
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
